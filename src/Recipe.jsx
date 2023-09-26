949b5ee04fcac5d68d523229623fc6aa84b240ba
Hicham Roldan <82762193+Hicham017@users.noreply.github.com>
2023-09-26 09:29:57 +0200

    couleur des titres et changement de la font

--- src/Recipe.jsx
+++ src/Recipe.jsx
@@ -4,7 +4,7 @@ import style from "./recipe.module.css";
 const Recipe = ({ title, calories, image, ingredients }) => {
   return (
     <div className={style.recipe}>
-      <h1>{title}</h1>
+      <h2>{title}</h2>
       <ol>
         {ingredients.map((ingredients) => (
           <li>{ingredients.text}</li>
